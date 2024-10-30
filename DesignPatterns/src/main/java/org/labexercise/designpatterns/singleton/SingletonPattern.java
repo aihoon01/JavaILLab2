6539e8ef63000186a4bfb95d170401eba65aa222
Stephen Paakow Aihoon <stephen.aihoon@amalitech.org>
2024-10-30 18:13:22 +0000

    modified singleton pattern

--- DesignPatterns/src/main/java/org/labexercise/designpatterns/singleton/SingletonPattern.java
+++ DesignPatterns/src/main/java/org/labexercise/designpatterns/singleton/SingletonPattern.java
@@ -2,11 +2,23 @@ package org.labexercise.designpatterns.singleton;
 
 public class SingletonPattern {
 
+    // Step 1: A private static instance of itself
+    private static SingletonPattern instance;
+
     private final Logger logger;
     private String definition = "Singleton method is a design pattern that has only one instance and provides a global access point";
 
-    public SingletonPattern(Logger logger) {
+    // Step 2: Private constructor to prevent instantiation
+    private SingletonPattern(Logger logger) {
         this.logger = logger;
+    }
+
+    // Step 3: Public static method to provide global access to the instance
+    public static synchronized SingletonPattern getInstance(Logger logger) {
+            if (instance == null) {
+                instance = new SingletonPattern(logger);
+        }
+        return instance;
     }
 
     public void defineSingleton() {
@@ -20,12 +32,12 @@ public class SingletonPattern {
     public void defineInLowerCase() {
         logger.logWithCapitals(definition, false);
     }
-
 }
 
 class Main {
     public static void main(String[] args) {
-        SingletonPattern sp = new SingletonPattern(new Logger());
+        Logger logger = new Logger();
+        SingletonPattern sp = SingletonPattern.getInstance(logger);
         sp.defineSingleton();
         sp.defineInUpperCase();
         sp.defineInLowerCase();
